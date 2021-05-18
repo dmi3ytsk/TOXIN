f2aaffee1e9200b9c7b82f1153187104943d7853
dmi3ytsk <dmi3y_tsk@bk.ru>
2021-05-18 11:56:56 +0200

    Add form elements page

---
+++ src/pages/form-elements/main.js
@@ -0,0 +1,44 @@
+import "../../../node_modules/jquery/dist/import-jquery.js";
+
+import "../../base.scss";
+import "../../vars.scss";
+
+import './form-elements.scss';
+
+import "../../blocks/textfield/textfield.scss";
+
+import "../../blocks/dropdown/dropdown.scss";
+import "../../blocks/dropdown/dropdown.js";
+
+import "../../blocks/datepicker/datepicker.scss";
+import "../../blocks/datepicker/datepicker.js";
+
+import "../../blocks/checkbox/checkbox.scss";
+
+import "../../blocks/radio/radio.scss";
+
+import "../../blocks/toggle/toggle.scss";
+
+import "../../blocks/likeBtn/likeBtn.scss";
+import "../../blocks/likeBtn/likeBtn.js";
+
+import "../../blocks/rateStars/rateStars.scss";
+import "../../blocks/rateStars/rateStars.js";
+
+import "../../blocks/rangeSlider/rangeSlider.scss";
+
+import "../../blocks/button/button.scss";
+
+import "../../blocks/pagination/pagination.scss";
+import "../../blocks/pagination/pagination.js";
+
+import "../../blocks/checkboxExpandList/checkboxExpandList.scss";
+import "../../blocks/checkboxExpandList/checkboxExpandList.js";
+
+import "../../blocks/richCheckbox/richCheckbox.scss";
+
+import "../../blocks/bulletList/bulletList.scss";
+
+import "../../blocks/aboutRoom/aboutRoom.scss";
+
+import "../../blocks/userFeedback/userFeedback.scss";
