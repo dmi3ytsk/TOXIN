eb5bd80b36bedd319c98def947fe54009cad105b
dmi3ytsk <dmi3y_tsk@bk.ru>
2021-05-22 21:00:25 +0200

    Update registration card

---
+++ src/blocks/cardRegistration/cardRegistration.js
@@ -0,0 +1,10 @@
+import "./cardRegistration.scss"
+
+import "../../blocks/textfield/textfield.scss";
+
+import "../../blocks/radio/radio.scss";
+
+import "../../blocks/toggle/toggle.scss";
+
+import "../../blocks/button/button.scss";
+
