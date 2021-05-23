5c10b1385bed927a0da823f25d3fc2db5b9a3ec3
dmi3ytsk <dmi3y_tsk@bk.ru>
2021-05-23 21:33:01 +0200

    Done cards page

---
+++ src/pages/cards/main.js
@@ -0,0 +1,16 @@
+import "../../../node_modules/jquery/dist/import-jquery.js";
+
+import "../../base.scss";
+import "../../vars.scss";
+
+import './cards.scss';
+
+import "../../blocks/cardFindRoom/findRoom.js";
+
+import "../../blocks/cardRegistration/cardRegistration.js";
+
+import "../../blocks/cardBuyRoom/cardBuyRoom.js";
+
+import "../../blocks/cardLogin/cardLogin.js";
+
+import "../../blocks/cardRoomThumbnail/cardRoomThumbnail.js";
