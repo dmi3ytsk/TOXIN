2e5a607eea2894ed5c7903256c35cf6ba0d80774
dmi3ytsk <dmi3y_tsk@bk.ru>
2021-05-25 23:34:53 +0200

    Done login page

---
+++ src/pages/login/main.js
@@ -0,0 +1,12 @@
+import "../../../node_modules/jquery/dist/import-jquery.js";
+
+import "../../base.scss";
+import "../../vars.scss";
+
+import './login.scss';
+
+import "../../blocks/header/header.js";
+
+import "../../blocks/footer/footer.js";
+
+import "../../blocks/cardLogin/cardLogin.js";
