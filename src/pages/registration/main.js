8810266bae947cbd48935c74773b7f1bd2ce1946
dmi3ytsk <dmi3y_tsk@bk.ru>
2021-05-25 16:42:13 +0200

    Done registraion page

---
+++ src/pages/registration/main.js
@@ -0,0 +1,12 @@
+import "../../../node_modules/jquery/dist/import-jquery.js";
+
+import "../../base.scss";
+import "../../vars.scss";
+
+import './registration.scss';
+
+import "../../blocks/header/header.js";
+
+import "../../blocks/footer/footer.js";
+
+import "../../blocks/cardRegistration/cardRegistration.js";
