89ca6d5cf1bb4f8046b1e2a03682e5cfcd589801
dmi3ytsk <dmi3y_tsk@bk.ru>
2021-05-29 12:57:41 +0200

    Add room search page

---
+++ src/pages/find-room/main.js
@@ -0,0 +1,34 @@
+import "../../../node_modules/jquery/dist/import-jquery.js";
+
+import "../../base.scss";
+import "../../vars.scss";
+
+import './find-room.scss';
+
+import "../../blocks/header/header.js";
+
+import "../../blocks/footer/footer.js";
+
+import "../../blocks/textfield/textfield.scss";
+
+import "../../blocks/dropdown/dropdown.scss";
+import "../../blocks/dropdown/dropdown.js";
+
+import "../../blocks/datepicker/datepicker.scss";
+import "../../blocks/datepicker/datepicker.js";
+
+import "../../blocks/checkbox/checkbox.scss";
+
+import "../../blocks/rangeSlider/rangeSlider.scss";
+
+import "../../blocks/pagination/pagination.scss";
+import "../../blocks/pagination/pagination.js";
+
+import "../../blocks/checkboxExpandList/checkboxExpandList.scss";
+import "../../blocks/checkboxExpandList/checkboxExpandList.js";
+
+import "../../blocks/richCheckbox/richCheckbox.scss";
+
+import "../../blocks/bulletList/bulletList.scss";
+
+import "../../blocks/cardRoomThumbnail/cardRoomThumbnail.js";
