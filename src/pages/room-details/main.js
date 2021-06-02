522b3eb0c442acf7fab32450984464b5982fc829
dmi3ytsk <dmi3y_tsk@bk.ru>
2021-06-02 22:39:18 +0200

    Done room #888 details page

---
+++ src/pages/room-details/main.js
@@ -0,0 +1,21 @@
+import "../../../node_modules/jquery/dist/import-jquery.js";
+
+import "../../base.scss";
+import "../../vars.scss";
+
+import "./room-details.scss";
+
+import "../../blocks/header/header.js";
+
+import "../../blocks/footer/footer.js";
+
+import "../../blocks/cardBuyRoom/cardBuyRoom.js";
+
+import "../../blocks/bulletList/bulletList.scss";
+
+import "../../blocks/aboutRoom/aboutRoom.scss";
+
+import "../../blocks/userFeedback/userFeedback.js";
+
+import "../../blocks/likeBtn/likeBtn.scss";
+
