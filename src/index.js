e5b58f21b05f9f2cd5d7b3227efb5124363ccd84
dmi3ytsk <dmi3y_tsk@bk.ru>
2021-06-25 20:55:51 +0200

    Delete unnecessary files

--- src/index.js
+++
@@ -1,8 +0,0 @@
-// Turn on jQuery
-import "../node_modules/jquery/dist/import-jquery";
-// Import JS from each item of page
-import './blocks/blocks.js';
-
-import './base.scss'; // Nullstyle + Variables + Mixins + Fonts
-import './blocks/blocks.scss'; // Style for each item of page
-import "./pages/pages.scss" // Style of page
