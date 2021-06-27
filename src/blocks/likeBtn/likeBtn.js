e8c26eb08f227fb0f9c38896482c9a20b6c4bd01
dmi3ytsk <dmi3y_tsk@bk.ru>
2021-06-27 15:49:38 +0200

    Bug fixing

--- src/blocks/likeBtn/likeBtn.js
+++ src/blocks/likeBtn/likeBtn.js
@@ -8,21 +8,21 @@ class LikeButton {
     const doc = this.element,
       btn = doc.querySelector(".likeBtn__design"),
       likes = doc.querySelector(".likeBtn__likes"),
+      status = doc.querySelector(".liked"),
       heart = doc.querySelector(".likeBtn__heart");
 
-    let like = true,
+    let isLiked = status ? false : true,
       likeCount = likes.innerHTML,
       heartIcon = heart.innerHTML;
-
+    
     btn.addEventListener("click", () => {
-      likeCount = like ? ++likeCount : --likeCount;
-      heartIcon = like ? "favorite" : "favorite_border";
-      // console.log(btn.classList)
+      likeCount = isLiked ? ++likeCount : --likeCount;
+      heartIcon = isLiked ? "favorite" : "favorite_border";
       btn.classList.toggle("liked");
       likes.classList.toggle("liked");
       heart.classList.toggle("liked");
 
-      like = !like;
+      isLiked = !isLiked;
       likes.innerHTML = likeCount;
       heart.innerHTML = heartIcon;
     });
