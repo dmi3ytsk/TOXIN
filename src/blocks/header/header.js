5738ad362d518da59b30a7f5bc1dacfcac837417
dmi3ytsk <dmi3y_tsk@bk.ru>
2022-07-25 22:26:23 +0200

    update hamburger menu

--- src/blocks/header/header.js
+++ src/blocks/header/header.js
@@ -1,1 +1,8 @@
+import HamburgerMenu from "../hamburger-menu/HamburgerMenu";
+
 import "./header.scss";
+
+const dropdown = document.querySelectorAll(".js-header__burger");
+dropdown.forEach(function (e) {
+  new HamburgerMenu(e);
+});
