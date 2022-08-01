1c3c36211436c531d3c6ab88fd50402b76f9248c
dmi3ytsk <dmi3y_tsk@bk.ru>
2022-08-01 11:55:53 +0200

    #11 simplification

--- src/blocks/hamburger-menu/HamburgerMenu.js
+++ src/blocks/hamburger-menu/HamburgerMenu.js
@@ -8,26 +8,18 @@ class HamburgerMenu {
   hamburger() {
     const button = document.querySelector(".js-hamburger-menu__button");
     const box = document.querySelector(".js-hamburger-menu__box");
-    button.innerHTML = "menu";
-
-    this.showBurgerButton(button);
 
     window.addEventListener("resize", () => this.showBurgerButton(button));
     button.addEventListener("click", () => this.toggleMenu(button, box));
   }
   toggleMenu(button, box) {
-    button.innerHTML === "menu"
-      ? (button.innerHTML = "close")
-      : (button.innerHTML = "menu");
+    if (button.innerHTML === "menu") {
+      button.innerHTML = "close"
+    } else {
+      button.innerHTML = "menu"
+    }
     box.classList.toggle("hamburger-menu__box_hide");
     button.classList.toggle("hamburger-menu__button_checked");
-  }
-  showBurgerButton(button) {
-    if (window.matchMedia("(max-width: 956px)").matches) {
-      button.classList.remove("hamburger-menu__button_hide");
-    } else {
-      button.classList.add("hamburger-menu__button_hide");
-    }
   }
 }
 export default HamburgerMenu;
